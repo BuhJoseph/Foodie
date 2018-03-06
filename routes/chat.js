3498e15b82b5e64d508742ea51e7748a71a67567
Jan Eric De Castro <jedecast.edu>
2018-03-06 14:15:38 -0800

    Added some pages and refined styling

--- routes/chat.js
+++ routes/chat.js
@@ -3,5 +3,40 @@
  */
 
 exports.view = function(req, res){
-  res.render('chat');
+
+	var name = req.params.name; 
+ 	var profile = "../images/matches/mark.jpg";
+
+ 	if (name == "Mark Zuckerberg") {
+    profile = "../images/matches/mark.jpg";
+    background = "../images/matches/facebook.jpg";
+  } else if (name == "Godfrey Gao") {
+    profile = "../images/matches/godfrey.jpg";
+    background = "../images/matches/hongkong.jpg";
+  } else if (name == "Chloe Kim") {
+    profile = "../images/matches/chloe.jpg";
+    background = "../images/matches/pyeongchang.jpg";
+  } else if (name == "Pradeep Coleslaw") {
+    profile = "../images/matches/khosla.jpg";
+    background = "../images/food/coleslaw.jpg";
+  } else if (name == "Michelle Obama") {
+    profile = "../images/matches/michelle.jpg";
+    background = "../images/matches/whitehouse.jpg";
+  } else if (name == "Udon Musk") {
+    profile = "../images/matches/elon.jpg";
+    background = "../images/food/udon.jpg";
+  } else if (name == "Jeff Basil") {
+    profile = "../images/matches/jeff.jpg";
+    background = "../images/food/basil.jpg";
+  } else if (name == "Steph Curry") {
+    profile = "../images/matches/steph.jpg";
+    background = "../images/food/lambtikkamasala.jpg";
+  } else {
+    profile = "../images/matches/don.jpg";
+    background = "../images/food/donburi.jpeg";
+  }
+    res.render('chat', {
+      "imageURL": profile,
+      "name": name
+  });
 };
